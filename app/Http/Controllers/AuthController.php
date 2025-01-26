3914687c2b523ee68b6bea2fd07635cf618e1d03
Darfik43 <darfik43@gmail.com>
2025-01-26 23:53:42 +0300

    feat: return userInfo after register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request) {
+    public function register(Request $request) : User {
         $incomingFields = $request->validate([
             'email' => ['required', 'email'],
-            'password' => ['required', 'min:5', 'max50'],
-            'passwordConfirmation' => ['required', 'min:5', 'max50']
+            'password' => ['required', 'min:5', 'max:50'],
+            'passwordConfirmation' => ['required', 'min:5', 'max:50']
         ]);
-        return 'test';
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+
+        return User::create($incomingFields);
     }
 }
