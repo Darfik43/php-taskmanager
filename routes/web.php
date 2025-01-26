a87eaff644dd7375f286152f6dabc8bd78dbe4bb
Darfik43 <darfik43@gmail.com>
2025-01-26 23:14:28 +0300

    feat: add route for authController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/api/v1/auth/register', [AuthController::class, 'register']);
