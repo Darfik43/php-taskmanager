5659912e425c456616389bf0f1a3aa153cd77dab
Darfik43 <darfik43@gmail.com>
2025-01-28 23:30:29 +0300

    feat: delete security

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,4 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('users', AuthController::class)
-    -> middleware(['auth:api', 'throttle:api']);
+Route::apiResource('users', AuthController::class);
