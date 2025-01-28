874451bc34cd728d1d2eb9bf74ed9f727b043b81
Darfik43 <darfik43@gmail.com>
2025-01-28 23:32:44 +0300

    chore: delete comment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
-            'api/*' // <-- exclude this route
+            'api/*'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
