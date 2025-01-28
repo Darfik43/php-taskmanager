0aa30933a727f3661aba3f256a17218473217d1b
Darfik43 <darfik43@gmail.com>
2025-01-28 21:27:30 +0300

    feat: delete home root from routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::post('/api/v1/auth/register',
     [AuthController::class, 'register']);
