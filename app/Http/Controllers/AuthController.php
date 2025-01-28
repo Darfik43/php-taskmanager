0d7b7b1032daae952e78fe9f93f622d392838ef9
Darfik43 <darfik43@gmail.com>
2025-01-28 21:26:22 +0300

    refactor: delete additional fields

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(SignupRequest $signupRequest) : User {
-        $incomingFields = $signupRequest->validated();
+        $signupRequest['password'] = bcrypt($signupRequest['password']);
 
-        $incomingFields['password'] = bcrypt($incomingFields['password']);
-
-        return User::create($incomingFields);
+        return User::create($signupRequest->validated());
     }
 }
