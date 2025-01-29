6a541bac180516edf694d7206ba8720a939077cd
Darfik43 <darfik43@gmail.com>
2025-01-29 21:37:21 +0300

    refactor: add automatic DI

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,8 @@ use App\Services\UserService;
 
 class UserController extends Controller
 {
-    private UserService $userService;
-
-    public function __construct(UserService $userService)
-    {
-        $this->userService = $userService;
-    }
+    public function __construct(private readonly UserService $userService)
+    {}
 
     public function store(SignupRequest $signupRequest) : UserResource
     {
