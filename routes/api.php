62883be0d9d3c858d45bb8f4aa9ba589464840f6
Darfik43 <darfik43@gmail.com>
2025-01-29 23:01:51 +0300

    feat: add route for login endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+
+Route::prefix('auth')->group(function (){
+    Route::post('login', [AuthController::class, 'login']);
+});
