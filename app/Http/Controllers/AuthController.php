e9f4cb49acb85c1b9f304f3899bc417cd328c095
Darfik43 <darfik43@gmail.com>
2025-01-29 22:57:54 +0300

    feat: implement logic of AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidCredentialsException;
 use App\Http\Requests\LoginRequest;
+use App\Http\Resources\AuthResource;
+use App\Services\AuthService;
 use App\Services\UserService;
 
 class AuthController extends Controller
 {
-    public function __construct(private readonly UserService $userService)
-    {
-    }
+    public function __construct(
+        private readonly AuthService $authService
+    ) {}
 
-    public function update(LoginRequest $loginRequest)
-    {
 
+    /**
+     * @throws InvalidCredentialsException
+     */
+    public function login(LoginRequest $loginRequest): AuthResource
+    {
+        try {
+            $response = $this->authService->login($loginRequest->validated());
+            return new AuthResource($response);
+        } catch (InvalidCredentialsException $e) {
+            throw $e; //TODO Mocked logic of throwing
+        }
     }
 }
