e9902c14ea8f0238e4f1fb72cca17acb99f44f95
Darfik43 <darfik43@gmail.com>
2025-01-29 22:25:45 +0300

    feat: add findByEmail method for userRepo

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -6,6 +6,10 @@ use App\Models\User;
 
 class UserRepository
 {
+    public function findByEmail(string $email) : User {
+        return User::whereEmail($email);
+    }
+
     public function create(array $data)
     {
         return User::create($data);
