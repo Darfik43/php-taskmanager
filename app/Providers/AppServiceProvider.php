4e1bef71789dac816045968e1c66cd228bace3fd
Darfik43 <darfik43@gmail.com>
2025-01-29 22:59:40 +0300

    feat: register DI bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\UserRepository;
+use App\Repositories\UserRepositoryImpl;
+use App\Services\AuthService;
+use App\Services\AuthServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRepository::class, UserRepositoryImpl::class);
+        $this->app->bind(AuthService::class, AuthServiceImpl::class);
     }
 
     /**
