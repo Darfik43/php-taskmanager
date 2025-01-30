f7bec700c1468cc1d8fd4fb572d47f971b77e883
Darfik43 <darfik43@gmail.com>
2025-01-30 22:57:33 +0300

    feat: add Route part to be protected

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,15 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
 
 Route::prefix('auth')->group(function (){
     Route::post('login', [AuthController::class, 'login']);
+});
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+   //TODO add routes that must be protected
 });
