a3399dea8c096f8b166400f293e425a645b5db8e
Darfik43 <darfik43@gmail.com>
2025-02-02 18:54:51 +0300

    feat: add route for tasks endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('tasks', TaskController::class);
 
 Route::prefix('auth')->group(function (){
     Route::post('login', [AuthController::class, 'login']);
