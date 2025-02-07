ef57f6c5b466c979385875891a6c560610551b98
Darfik43 <darfik43@gmail.com>
2025-02-07 20:46:10 +0300

    feat: create columns for refresh tokens

--- database/migrations/2025_02_07_170828_create_refresh_tokens_table.php
+++ database/migrations/2025_02_07_170828_create_refresh_tokens_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('refresh_tokens', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('token');
+            $table->timestamps('expires_at');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
