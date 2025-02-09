72485a731d8fabc56878e12167df05f581ed71fd
Darfik43 <darfik43@gmail.com>
2025-02-10 01:27:27 +0300

    feat: add implementation for JWTService

---
+++ app/Services/JWTServiceImpl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTServiceImpl implements JWTService
+{
+
+    public function generateTokens($user): array
+    {
+        // TODO: Implement generateTokens() method.
+    }
+
+
+
+}
