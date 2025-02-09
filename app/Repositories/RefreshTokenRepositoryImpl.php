22b80fe0e3bf5081bc6dcf48e804c7f072e0891c
Darfik43 <darfik43@gmail.com>
2025-02-09 20:33:27 +0300

    feat: create implementation for RefreshTokenRepository

---
+++ app/Repositories/RefreshTokenRepositoryImpl.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories;
+
+class RefreshTokenRepositoryImpl implements RefreshTokenRepository
+{
+
+    public function create(string $token)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function find(string $token)
+    {
+        // TODO: Implement find() method.
+    }
+}
