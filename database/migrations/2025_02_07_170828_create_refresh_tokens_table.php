d2614c4a92df98de2d4d70f8ef11aa84895c0eb0
Darfik43 <darfik43@gmail.com>
2025-02-10 22:44:20 +0300

    feat: add created_at field for refresh tokens

--- database/migrations/2025_02_07_170828_create_refresh_tokens_table.php
+++ database/migrations/2025_02_07_170828_create_refresh_tokens_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('refresh_tokens', function (Blueprint $table) {
             $table->id();
             $table->string('token');
+            $table->timestamp('created_at');
             $table->timestamp('expires_at');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
