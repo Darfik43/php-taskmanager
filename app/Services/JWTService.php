4b7f3ece4786f35d2afde51fe9f9f1adc80cbeb9
Darfik43 <darfik43@gmail.com>
2025-02-16 21:47:02 +0300

    feat: add JWTSubject to refreshTokens signature

--- app/Services/JWTService.php
+++ app/Services/JWTService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
 interface JWTService
 {
     public function generateTokens($user): array;
 
-    public function refreshTokens(string $token): array;
+    public function refreshTokens(string $token, JWTSubject $user): array;
 }
