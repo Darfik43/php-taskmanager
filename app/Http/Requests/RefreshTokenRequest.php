b2c02af4b4c2a5037f3526b60fcf67e72a3feee6
Darfik43 <darfik43@gmail.com>
2025-02-16 21:55:43 +0300

    feat: add required for refreshToken request

--- app/Http/Requests/RefreshTokenRequest.php
+++ app/Http/Requests/RefreshTokenRequest.php
@@ -22,7 +22,7 @@ class RefreshTokenRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'refreshToken' => 'required'
         ];
     }
 }
