dcdc2f3214c96e855b9889f7ea3455dace85830b
Darfik43 <darfik43@gmail.com>
2025-02-16 18:40:18 +0300

    feat: add method to refresh Tokens

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -6,5 +6,7 @@ use App\Models\User;
 
 interface AuthService
 {
-    public function login(array $credentials) : array;
+    public function login(array $credentials): array;
+
+    public function refreshTokens(string $token): array;
 }
