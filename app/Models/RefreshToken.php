fad5c482dac30bdc04d218ebd8c791f4bf7c42a5
Darfik43 <darfik43@gmail.com>
2025-02-19 00:03:19 +0300

    chore: add phpdoc

--- app/Models/RefreshToken.php
+++ app/Models/RefreshToken.php
@@ -6,16 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- *
+ * 
  *
  * @property int $id
  * @property string $token
+ * @property string $created_at
  * @property string $expires_at
  * @property int $user_id
  * @property-read \App\Models\TFactory|null $use_factory
  * @method static \Illuminate\Database\Eloquent\Builder<static>|RefreshToken newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|RefreshToken newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|RefreshToken query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|RefreshToken whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|RefreshToken whereExpiresAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|RefreshToken whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|RefreshToken whereToken($value)
