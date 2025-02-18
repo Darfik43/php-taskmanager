f2b207bd3329a1933c07b0fb5fc0d346a1c285b8
Darfik43 <darfik43@gmail.com>
2025-02-19 00:20:27 +0300

    chore: add comment

--- app/Services/JWTServiceImpl.php
+++ app/Services/JWTServiceImpl.php
@@ -20,7 +20,7 @@ class JWTServiceImpl implements JWTService
     {
     }
 
-    public function generateTokens($user): array
+    public function generateTokens($user): array //TODO Split it into 2 methods, generate must be only generating method with no included of storing logic
     {
         $accessToken = $this->generateAccessToken($user);
         $refreshToken = $this->generateRefreshToken($user);
