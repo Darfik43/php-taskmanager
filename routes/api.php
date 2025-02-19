b060153772bccb549b633bcf33887d3207fa19e3
Darfik43 <darfik43@gmail.com>
2025-02-20 00:36:29 +0300

    docs: config

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtMiddleware;
+use App\Http\Middleware\RefreshTokenMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
@@ -11,8 +12,10 @@ Route::apiResource('tasks', TaskController::class);
 
 Route::prefix('auth')->group(function (){
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('refresh', [AuthController::class, 'refresh'])
+    ->middleware(RefreshTokenMiddleware::class);
 });
 
 Route::middleware([JwtMiddleware::class])->group(function () {
-   Route::apiResource('tasks', TaskController::class);
+//   Route::apiResource('tasks', TaskController::class);
 });
