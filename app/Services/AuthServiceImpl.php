b05a5f39dfdd630d14b1eaeda16ca50dc2c9ac7e
Darfik43 <darfik43@gmail.com>
2025-02-20 00:36:34 +0300

    Revert "feat: delete user from params"
    This reverts commit cf67798eb06683993954db1a86ba9e25bf05dc8d.

--- app/Services/AuthServiceImpl.php
+++ app/Services/AuthServiceImpl.php
@@ -34,6 +34,11 @@ class AuthServiceImpl implements AuthService
      */
     public function refreshTokens(string $token): array
     {
-        return $this->jwtService->refreshTokens($token);
+        $user = JWTAuth::setToken($token)->authenticate();
+        if ($user) {
+            return $this->jwtService->refreshTokens($token, $user);
+        } else {
+            throw new InvalidTokenException("Invalid token");
+        }
     }
 }
