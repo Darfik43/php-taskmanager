7a65116b3729ae7c54b5b0def7944394258a876c
Darfik43 <darfik43@gmail.com>
2025-02-20 01:06:53 +0300

    refactor: code clean up

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -5,7 +5,6 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
