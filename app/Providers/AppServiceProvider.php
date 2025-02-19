110240605067a5174cc12f2fc130bbb92066a7bc
Darfik43 <darfik43@gmail.com>
2025-02-19 23:41:11 +0300

    feat: add service in provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,8 @@ use App\Services\AuthService;
 use App\Services\AuthServiceImpl;
 use App\Services\JWTService;
 use App\Services\JWTServiceImpl;
+use App\Services\UserService;
+use App\Services\UserServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(UserRepository::class, UserRepositoryImpl::class);
+        $this->app->bind(UserService::class, UserServiceImpl::class);
         $this->app->bind(AuthService::class, AuthServiceImpl::class);
         $this->app->bind(JWTService::class, JWTServiceImpl::class);
         $this->app->bind(RefreshTokenRepository::class, RedisRefreshTokenRepository::class);
