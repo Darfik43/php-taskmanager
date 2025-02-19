ff7aebe3eee28b463c9b13918c04aeddcf0c758b
Darfik43 <darfik43@gmail.com>
2025-02-19 22:58:12 +0300

    feat: create middleware to validate token

---
+++ app/Http/Middleware/RefreshTokenMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class RefreshTokenMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        try {
+            JWTAuth::setToken($request['refreshToken'])->parseToken()->authenticate();
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Token is not valid'], 400);
+        }
+
+        return $next($request);
+    }
+}
