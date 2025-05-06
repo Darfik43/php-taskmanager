847cb007031dfbc71fc23e59f664501c04f24fca
darfik43 <darfik43@gmail.com>
2025-05-07 00:03:36 +0300

    feat: add mailjet dependency

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,7 @@
 <?php
+use Mailjet\LaravelMailjet\MailjetServiceProvider;
 
 return [
     App\Providers\AppServiceProvider::class,
+    MailjetServiceProvider::class,
 ];
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtMiddleware;
 use App\Http\Middleware\RefreshTokenMiddleware;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
@@ -14,6 +15,10 @@ Route::prefix('auth')->group(function (){
     Route::post('login', [AuthController::class, 'login']);
     Route::post('refresh', [AuthController::class, 'refresh']);
 });
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+})->name('verification.verify');
 
 Route::middleware([JwtMiddleware::class])->group(function () {
 //   Route::apiResource('tasks', TaskController::class);
