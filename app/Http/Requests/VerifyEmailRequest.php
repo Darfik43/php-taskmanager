a2030f29aa28041ed26d956d130917b8bab50f4e
darfik43 <darfik43@gmail.com>
2025-05-12 19:50:13 +0300

    fix: change how email verification request is generated

--- app/Http/Requests/VerifyEmailRequest.php
+++ app/Http/Requests/VerifyEmailRequest.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Requests;
 
-use App\Exceptions\EmailAlreadyVerifiedException;
-use App\Exceptions\VerificationInvalidLinkException;
-use App\Models\User;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Http\FormRequest;
 
 class VerifyEmailRequest extends FormRequest
@@ -26,25 +22,16 @@ class VerifyEmailRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'id' => 'required|integer|exists:users,id',
+            'hash' => 'required|string',
         ];
     }
 
-    public function fulfill(): void
+    public function all($keys = null): array
     {
-        $user = User::find($this->route('id'));
-
-        if (!$user || !hash_equals(
-            sha1($user->getEmailForVerification()),
-            (string) $this->route('hash')
-            )) {
-            throw new VerificationInvalidLinkException('Invalid verification link');
-        }
-
-        if ($user->hasVerifiedEmail()) {
-            throw new EmailAlreadyVerifiedException('Email already verified');
-        }
-
-        $user->markEmailAsVerified();
+        $data = parent::all($keys);
+        $data['id'] = $this->route('id');
+        $data['hash'] = $this->route('hash');
+        return $data;
     }
 }
