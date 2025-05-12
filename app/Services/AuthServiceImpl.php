0739d5237306e1d7f7f37da643c426acbf06d222
darfik43 <darfik43@gmail.com>
2025-05-12 20:32:45 +0300

    feat: add method to check if email is verified. If not we do not pass user

--- app/Services/AuthServiceImpl.php
+++ app/Services/AuthServiceImpl.php
@@ -2,9 +2,10 @@
 
 namespace App\Services;
 
+use App\Exceptions\EmailNotVerifiedException;
 use App\Exceptions\InvalidCredentialsException;
 use App\Exceptions\InvalidTokenException;
-use App\Repositories\UserRepository;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -12,16 +13,22 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class AuthServiceImpl implements AuthService
 {
     public function __construct(
-        private readonly UserRepository $userRepository,
-        private readonly JWTService $jwtService
-    ) {}
+        private readonly UserService $userService,
+        private readonly JWTService     $jwtService
+    )
+    {
+    }
 
+    /**
+     * @throws EmailNotVerifiedException
+     * @throws InvalidCredentialsException
+     */
     public function login(array $credentials): array
     {
-        $user = $this->userRepository->findByEmail($credentials['email']);
+        $user = $this->getAuthenticatingUser($credentials);
 
-        if (!$user || !Hash::check($credentials['password'], $user->password)) {
-            throw new InvalidCredentialsException('Invalid credentials');
+        if (!$this->isEmailVerified($user)) {
+            throw new EmailNotVerifiedException('Email not verified');
         }
 
         return $this->jwtService->generateTokens($user);
@@ -36,5 +43,28 @@ class AuthServiceImpl implements AuthService
         } catch (JWTException $e) {
             throw new InvalidTokenException("Invalid token: " . $e->getMessage());
         }
+    }
+
+    /**
+     * @throws InvalidCredentialsException
+     */
+    private function getAuthenticatingUser(array $credentials): User
+    {
+        $user = $this->userService->getUserByEmail($credentials['email']);
+
+        if (!$this->isPasswordValid($user, $credentials['password'])) {
+            throw new InvalidCredentialsException('Invalid credentials');
+        }
+
+        return $user;
+    }
+    private function isPasswordValid(?User $user, string $password): bool
+    {
+        return $user && Hash::check($password, $user->password);
+    }
+
+    private function isEmailVerified(User $user): bool
+    {
+        return $this->userService->isEmailVerified($user['id']);
     }
 }
