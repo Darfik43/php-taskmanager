5458cd5c0f6245c9b5533bf3f0ef9c0d565e9c0b
darfik43 <darfik43@gmail.com>
2025-05-12 19:48:53 +0300

    feat: change returning message

--- app/Http/Resources/EmailVerificationResource.php
+++ app/Http/Resources/EmailVerificationResource.php
@@ -14,6 +14,8 @@ class EmailVerificationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'success' => true,
+        ];
     }
 }
