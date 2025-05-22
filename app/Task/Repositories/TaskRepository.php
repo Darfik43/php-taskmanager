60409e54afbee59960da6a39b93e56557eb6d5f8
darfik43 <darfik43@gmail.com>
2025-05-22 23:25:04 +0300

    fix: update methods parameters

--- app/Task/Repositories/TaskRepository.php
+++ app/Task/Repositories/TaskRepository.php
@@ -8,7 +8,7 @@ interface TaskRepository
 {
     public function create(Task $task): Task;
     public function update(Task $task): Task;
-    public function delete(int $id): void;
-    public function findById(int $id): Task;
+    public function delete(int $id): bool;
+    public function findById(int $id): ?Task;
     public function findAll(int $user_id): array;
 }
