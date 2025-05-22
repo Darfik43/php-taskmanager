57107d67ba7245924f719b5f25a6d2b339cdd20e
darfik43 <darfik43@gmail.com>
2025-05-23 01:01:18 +0300

    feat: create TaskService implementaion

---
+++ app/Task/Services/TaskServiceImpl.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Task\Services;
+
+use App\Task\TaskDTO;
+use App\Task\TaskService;
+
+class TaskServiceImpl implements TaskService
+{
+    public function create(TaskDTO $taskDTO): bool
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update(TaskDTO $taskDTO): bool
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete(TaskDTO $taskDTO): bool
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function get(int $id): TaskDTO
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function getAll(int $user_id): array
+    {
+        // TODO: Implement getAll() method.
+    }
+
+}
