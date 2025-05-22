db2f2226bde8e5166f91b1d42610f6e686288030
darfik43 <darfik43@gmail.com>
2025-05-23 00:41:33 +0300

    feat: change returning type

--- app/Task/Repositories/TaskRepository.php
+++ app/Task/Repositories/TaskRepository.php
@@ -8,7 +8,7 @@ interface TaskRepository
 {
     public function create(Task $task): bool;
     public function update(Task $task): int;
-    public function delete(int $id): bool;
+    public function delete(int $id): int;
     public function findById(int $id): ?Task;
     public function findAll(int $user_id): array;
 }
