a016aff8a2b67ae7d62b97f14e8ffb190096baf7
darfik43 <darfik43@gmail.com>
2025-05-25 22:44:54 +0300

    feat: create Taskcontroller

---
+++ app/Task/Http/Controllers/TaskController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Task\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Task\TaskService;
+
+class TaskController extends Controller
+{
+    public function __construct(
+        private readonly TaskService $taskService
+    ) {}
+
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
