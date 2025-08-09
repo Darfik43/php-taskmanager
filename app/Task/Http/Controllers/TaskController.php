44c43c40b533a0e800c91d369e888c63d4761a09
Darfik43 <darfik43@gmail.com>
2025-08-09 19:27:57 +0200

    feat: implement store update and destroy methods

--- app/Task/Http/Controllers/TaskController.php
+++ app/Task/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Task\Http\Controllers;
 
 use App\Authentication\Http\Controllers\Controller;
+use App\Task\DTOs\TaskDTO;
 use App\Task\Http\Requests\CreateTaskRequest;
 use App\Task\Http\Requests\UpdateTaskRequest;
 use App\Task\Http\Resources\ShortTaskCollection;
@@ -26,18 +27,18 @@ class TaskController extends Controller
         return new TaskResource($this->taskService->get($id, auth()->id()));
     }
 
-    public function store(CreateTaskRequest $createTaskRequest)
+    public function store(CreateTaskRequest $createTaskRequest): void
     {
-
+        $this->taskService->create(TaskDTO::make($createTaskRequest->toArray()));
     }
 
-    public function update(UpdateTaskRequest $updateTaskRequest)
+    public function update(UpdateTaskRequest $updateTaskRequest): void
     {
-
+        $this->taskService->update(TaskDTO::make($updateTaskRequest->toArray()));
     }
 
-    public function destroy($id)
+    public function destroy($id): void
     {
-
+        $this->taskService->delete($id, auth()->id());
     }
 }
